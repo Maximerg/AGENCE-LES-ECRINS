3279117e96e2494dc834d85c45845f5b4cc0dbe2
Maximerg <155920154+Maximerg@users.noreply.github.com>
2025-01-29 21:53:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const footer = document.querySelector(".footer-fixed");
 
-    window.addEventListener("scroll", function() {
-        const scrollPosition = window.scrollY + window.innerHeight;
-        const pageHeight = document.documentElement.scrollHeight;
-
-        if (scrollPosition >= pageHeight) {
-            footer.classList.add("footer-bottom"); // Footer devient normal en bas de page
-        } else {
-            footer.classList.remove("footer-bottom"); // Footer reste fixe en navigation
-        }
-    });
-});
